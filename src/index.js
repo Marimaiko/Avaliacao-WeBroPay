d6ff0f5aa796fe929496dd2096c6b24637695bab
MarianaMaiko <marianamaikoht@gmail.com>
2020-08-27 21:19:28 -0300

    Implementação do swagger

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {swaggerConfig} = require('./middlewares/swagger')
 const routes = require('./routes.js');
 
 const app = express();
@@ -6,6 +7,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use(...swaggerConfig);
 
 app.get('/', (req, res) => res.json('Health check ok'));
 
